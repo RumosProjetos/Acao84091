d054f15018991d24d4a7801473ef2fe361720eca
Marcio Laclau <fialhomello@gmail.com>
2023-12-06 22:03:09 +0000

    marcio mello

---
+++ src/Compartilhado/marciomello.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Hello Marcio Mello, World!");
+Console.WriteLine("Hello Marcio Mello, World!");
+Console.WriteLine("Hello Marcio Mello, World!");
+Console.WriteLine("Hello Marcio Mello, World!");
+
+Console.WriteLine("Hello Marcio Mello 123, World!");
