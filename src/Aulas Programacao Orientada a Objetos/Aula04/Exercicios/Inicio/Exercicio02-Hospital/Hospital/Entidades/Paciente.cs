d6c9fae7104ef5feb2f4c53d1e8617fce09f6930
Jônatas Afonso Neto <bpshift001@bancoctt.pt>
2024-01-03 18:35:35 +0000

    Exemplo hospital

---
+++ src/Aulas Programacao Orientada a Objetos/Aula04/Exercicios/Inicio/Exercicio02-Hospital/Hospital/Entidades/Paciente.cs
@@ -0,0 +1,17 @@
+﻿namespace Hospital.Entidades;
+
+public class Paciente : Pessoa
+{
+    public string HistoricoClinico { get; set; }
+
+    public Paciente(string nome, int idade, string historicoClinico) : base(nome, idade)
+    {
+        HistoricoClinico = historicoClinico;
+    }
+
+    public override void ExibirInformacoes()
+    {
+        base.ExibirInformacoes();
+        Console.WriteLine($"Histórico Clínico: {HistoricoClinico}");
+    }
+}
