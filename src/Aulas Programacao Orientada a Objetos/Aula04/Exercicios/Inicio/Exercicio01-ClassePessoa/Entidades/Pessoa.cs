6960be9430a7f459937f282d8729235857ae1aaa
Jônatas Afonso Neto <bpshift001@bancoctt.pt>
2024-01-03 18:02:21 +0000

    Inicio da classe pessoa

---
+++ src/Aulas Programacao Orientada a Objetos/Aula04/Exercicios/Inicio/Exercicio01-ClassePessoa/Entidades/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio01_ClassePessoa.Entidades;
+
+internal class Pessoa
+{
+    // Atributos da classe
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+
+    // Construtor da classe
+    public Pessoa(string nome, int idade)
+    {
+        Nome = nome; 
+        Idade = idade;
+    }
+
+    // Método para exibir informações sobre a pessoa
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Nome: {Nome}, Idade: {Idade} anos.");
+    }
+}
--- src/Aulas Programacao Orientada a Objetos/Aula04/Exercicios/Inicio/Exercicio01-ClassePessoa/Program.cs
+++ src/Aulas Programacao Orientada a Objetos/Aula04/Exercicios/Inicio/Exercicio01-ClassePessoa/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Exercicio01_ClassePessoa.Entidades;
+
 Console.WriteLine("Hello, World!");
+
+
+// Exemplo de uso da classe Pessoa
+Pessoa pessoa1 = new Pessoa("João", 25);
+pessoa1.ExibirInformacoes();
+
+Pessoa pessoa2 = new Pessoa("Maria", 30);
+pessoa2.ExibirInformacoes();
