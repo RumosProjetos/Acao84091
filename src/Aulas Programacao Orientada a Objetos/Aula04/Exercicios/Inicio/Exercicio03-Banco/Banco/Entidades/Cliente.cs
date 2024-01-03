b2a620d1533946e79bb1dec02cbbcd990a25fd6c
Jônatas Afonso Neto <bpshift001@bancoctt.pt>
2024-01-03 18:43:03 +0000

    INicio do banco

---
+++ src/Aulas Programacao Orientada a Objetos/Aula04/Exercicios/Inicio/Exercicio03-Banco/Banco/Entidades/Cliente.cs
@@ -0,0 +1,18 @@
+﻿namespace Banco.Entidades;
+
+public class Cliente
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+
+    public Cliente(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+
+    public virtual void ExibirInformacoes()
+    {
+        Console.WriteLine($"Nome: {Nome}, Idade: {Idade} anos.");
+    }
+}
---
+++ src/Aulas Programacao Orientada a Objetos/Aula04/Exercicios/Inicio/Exercicio03-Banco/Banco/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Hello, World!");
+
+//TODO: falta completar
