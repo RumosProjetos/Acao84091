1d0e6f73e8aa509b7a28f4228e03c98dce84f533
Jonatas Afonso <jonatas_afonso@hotmail.com>
2024-01-05 20:00:51 +0000

    Exemplo inicio exception

---
+++ src/Aulas Programacao Orientada a Objetos/Aula05/TratamentoExceptions/TratamentoExceptions/Program.cs
@@ -0,0 +1,44 @@
+﻿using TratamentoExceptions;
+
+List<Funcionario> funcionariosDaEmpresa = new List<Funcionario>();
+
+for (int i = 0; i < 1000; i++)
+{
+    Funcionario func = new Funcionario
+    {
+        Nome = "Funcionario" + i,
+        Salario = i * 1000,
+        TaxaDoSeguro = i
+    };
+    funcionariosDaEmpresa.Add(func);
+}
+
+
+foreach (var item in funcionariosDaEmpresa)
+{
+    //gerar pagamento
+    item.SalarioLiquido();
+    item.CalcularSeguroObrigatorio();
+}
+
+
+string condicao = "Verdadeiro";
+
+if (condicao == "Verdadeiro")
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine("Funcionou");
+        Console.WriteLine("Linha 02");
+        Console.WriteLine("Linha 03");
+    }
+}
+
+try
+{
+    string conteudo = File.ReadAllText(@"c:\temp\temp.txt");
+}
+catch (Exception)
+{
+
+}
