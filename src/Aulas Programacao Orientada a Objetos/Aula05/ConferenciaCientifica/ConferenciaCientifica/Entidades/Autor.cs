7f9468115abc8623cdb39aec5000fb3b7f15bd8d
Jonatas Afonso <jonatas_afonso@hotmail.com>
2024-01-05 21:47:43 +0000

    Inicio Exemplo

---
+++ src/Aulas Programacao Orientada a Objetos/Aula05/ConferenciaCientifica/ConferenciaCientifica/Entidades/Autor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferenciaCientifica.Entidades
+{
+    internal class Autor : Pessoa
+    {
+        public string Especialidade { get; set; }
+        public string Filiacao { get; set; }
+    }
+}
---
+++ src/Aulas Programacao Orientada a Objetos/Aula05/ConferenciaCientifica/ConferenciaCientifica/Entidades/Palestra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferenciaCientifica.Entidades
+{
+    internal class Palestra
+    {
+        Palestrante Palestrante { get; set; }   
+        Artigo Artigo { get; set; }
+        DateTime? DataEHora { get; set; }
+
+        //...
+    }
+}
